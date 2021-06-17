5c175d7de6fce96df01c77e87dcdff8a1ea71964
Andrey Vlasov <59191751+Andrei-Vlasov@users.noreply.github.com>
2021-06-17 18:20:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ browser = webdriver.Chrome()
 browser.get("https://eldorado.ua/")
 browser.implicitly_wait(3)
 
-search_value = "телевизор"
-browser.find_element_by_css_selector('[name="new-search"]').send_keys(search_value)
+SEARCH_VALUE = "телевизор"
+browser.find_element_by_css_selector('[name="new-search"]').send_keys(SEARCH_VALUE)
 browser.find_element_by_css_selector('[name="new-search"]').send_keys(Keys.RETURN)
 actual_result = browser.find_element_by_css_selector('.search-top h5 span').text.lower()
-expected_result = search_value
+expected_result = SEARCH_VALUE
 
 assert actual_result == expected_result
